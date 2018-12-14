092704cdc2880fd700b0eae7521e918d3d7fac81
Harry <hsnetzer@gmail.com>
2018-12-14 18:38:55 -0500

    init

---
+++ BirdBush+Geo.swift
@@ -0,0 +1,193 @@
+//
+//  GeoBirdBush.swift
+//
+//  Created by Harry Netzer on 12/11/18.
+//
+//
+//  ISC License
+//
+//  Copyright (c) 2018, Vladimir Agafonkin
+//
+//  Permission to use, copy, modify, and/or distribute this software for any purpose
+//  with or without fee is hereby granted, provided that the above copyright notice
+//  and this permission notice appear in all copies.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+//  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+//  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+//  OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+//  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+//  THIS SOFTWARE.
+//
+
+import Foundation
+
+struct Earth {
+    static let radius: Double = 6371000 // meters
+}
+
+extension BirdBush {
+    private struct QueueElement: Comparable {
+        static func < (lhs: BirdBush.QueueElement, rhs: BirdBush.QueueElement) -> Bool {
+            return lhs.dist < rhs.dist
+        }
+        
+        static func == (lhs: BirdBush.QueueElement, rhs: BirdBush.QueueElement) -> Bool {
+            return lhs.dist == rhs.dist
+        }
+        
+        let dist: Double
+        let type: QueueElementType
+    }
+    
+    private enum QueueElementType {
+        case point(id: Int)
+        case node(left: Int, right: Int, axis: Int, minLon: Double, maxLon: Double, minLat: Double, maxLat: Double)
+    }
+    
+    func around(lon: Double, lat: Double, maxResults: Int = Int.max, maxDistance: Double = Double.greatestFiniteMagnitude) -> [Int] {
+        var maxHaverSinDist = 1.0, result = [Int]()
+        
+        if (maxDistance != Double.greatestFiniteMagnitude) {
+            maxHaverSinDist = haverSin(maxDistance / Earth.radius)
+        }
+        
+        // a distance-sorted priority queue that will contain both points and kd-tree nodes
+        var q = PriorityQueue<QueueElement>(ascending: true)
+        
+        // an object that represents the top kd-tree node (the whole Earth)
+        var node: QueueElementType? = QueueElementType.node(left: 0, right: ids.count - 1, axis: 0, minLon: -180, maxLon: 180, minLat: -90, maxLat: 90)
+        
+        let cosLat = cos(lat.degreesToRadians)
+        
+        while let noder = node {
+            switch noder {
+            case let .node(left, right, axis, minLon, maxLon, minLat, maxLat):
+                if (right - left <= nodeSize) { // leaf node
+                    
+                    // add all points of the leaf node to the queue
+                    for i in left...right {
+                        let item = ids[i]
+                        let dist = haverSinDist(lon1: lon, lat1: lat, lon2: coords[2 * i], lat2: coords[2 * i + 1], cosLat1: cosLat)
+                        q.push(QueueElement(dist: dist, type: QueueElementType.point(id: item)))
+                    }
+                } else { // not a leaf node (has child nodes)
+                    
+                    let m = (left + right) >> 1 // middle index
+                    let midLon = coords[2 * m]
+                    let midLat = coords[2 * m + 1]
+                    
+                    // add middle point to the queue
+                    let item = ids[m]
+                    let dist = haverSinDist(lon1: midLon, lat1: midLat, lon2: lon, lat2: lat, cosLat1: cosLat)
+                    q.push(QueueElement(dist: dist, type: QueueElementType.point(id: item)))
+                    
+                    let nextAxis = 1 - axis
+                    
+                    let nextMinLon = axis == 0 ? midLon : minLon
+                    let nextMaxLon = axis == 0 ? midLon : maxLon
+                    let nextMinLat = axis == 1 ? midLat : minLat
+                    let nextMaxLat = axis == 1 ? midLat : maxLat
+                    
+                    let leftDist = boxDist(qLon: lon, qLat: lat, cosLat: cosLat, minLon: minLon, maxLon: nextMaxLon, minLat: minLat, maxLat: nextMaxLat)
+                    let rightDist = boxDist(qLon: lon, qLat: lat, cosLat: cosLat, minLon: nextMinLon, maxLon: maxLon, minLat: nextMinLat, maxLat: maxLat)
+                    
+                    // first half of the node (check these)
+                    let leftNode = QueueElementType.node(left: left,
+                                                         right: m - 1,
+                                                         axis: nextAxis,
+                                                         minLon: minLon,
+                                                         maxLon: nextMaxLon,
+                                                         minLat: minLat,
+                                                         maxLat: nextMaxLat)
+                    // second half of the node
+                    let rightNode = QueueElementType.node(left: m + 1,
+                                                          right: right,
+                                                          axis: nextAxis,
+                                                          minLon: nextMinLon,
+                                                          maxLon: maxLon,
+                                                          minLat: nextMinLat,
+                                                          maxLat: maxLat)
+                    
+                    
+                    // add child nodes to the queue
+                    q.push(QueueElement(dist: leftDist, type: leftNode))
+                    q.push(QueueElement(dist: rightDist, type: rightNode))
+                }
+                
+                // fetch closest points from the queue; they're guaranteed to be closer
+                // than all remaining points (both individual and those in kd-tree nodes),
+                // since each node's distance is a lower bound of distances to its children
+                while let peek = q.peek() {
+                    if case .point(let id) = peek.type {
+                        let candidate = q.pop()!
+                        if (candidate.dist > maxHaverSinDist) { return result }
+                        result.append(id)
+                        if (result.count == maxResults) { return result }
+                    } else {
+                        break
+                    }
+                }
+                
+                // the next closest kd-tree node
+                node = q.pop()?.type
+            default:
+                print("error not a node")
+                break
+            }
+        }
+        return result
+    }
+    
+    // lower bound for distance from a location to points inside a bounding box
+    private func boxDist(qLon: Double, qLat: Double, cosLat: Double, minLon: Double, maxLon: Double, minLat: Double, maxLat: Double) -> Double {
+        // query point is between minimum and maximum longitudes
+        if (qLon >= minLon && qLon <= maxLon) {
+            if (qLat < minLat) { return haverSin((qLat - minLat).degreesToRadians) }
+            if (qLat > maxLat) { return haverSin((qLat - maxLat).degreesToRadians) }
+            return 0
+        }
+        
+        // query point is west or east of the bounding box;
+        // calculate the extremum for great circle distance from query point to the closest longitude;
+        let haverSinDLon = min(haverSin((qLon - minLon).degreesToRadians), haverSin((qLon - maxLon).degreesToRadians))
+        let extremumLat = vertexLat(lat: qLat, haverSinDLon: haverSinDLon)
+        
+        // if extremum is inside the box, return the distance to it
+        if (extremumLat > minLat && extremumLat < maxLat) {
+            return haverSinDistPartial(haverSinDLon: haverSinDLon, cosLat1: cosLat, lat1: qLat, lat2: extremumLat)
+        }
+        
+        // otherwise return the distan e to one of the bbox corners (whichever is closest)
+        return min(
+            haverSinDistPartial(haverSinDLon: haverSinDLon, cosLat1: cosLat, lat1: qLat, lat2: minLat),
+            haverSinDistPartial(haverSinDLon: haverSinDLon, cosLat1: cosLat, lat1: qLat, lat2: maxLat)
+        )
+    }
+    
+    private func haverSin(_ theta: Double) -> Double {
+        let s = sin(theta / 2)
+        return s * s
+    }
+    
+    private func haverSinDistPartial(haverSinDLon: Double, cosLat1: Double, lat1: Double, lat2: Double) -> Double {
+        return cosLat1 * cos(lat2.degreesToRadians) * haverSinDLon + haverSin((lat1 - lat2).degreesToRadians)
+    }
+    
+    private func haverSinDist(lon1: Double, lat1: Double, lon2: Double, lat2: Double, cosLat1: Double) -> Double {
+        let haverSinDLon = haverSin((lon1 - lon2).degreesToRadians)
+        return haverSinDistPartial(haverSinDLon: haverSinDLon, cosLat1: cosLat1, lat1: lat1, lat2: lat2)
+    }
+    
+    func distance(lon1: Double, lat1: Double, lon2: Double, lat2: Double) -> Double {
+        let h = haverSinDist(lon1: lon1, lat1: lat1, lon2: lon2, lat2: lat2, cosLat1: cos(lat1.degreesToRadians))
+        return 2 * Earth.radius * asin(sqrt(h))
+    }
+    
+    private func vertexLat(lat: Double, haverSinDLon: Double) -> Double {
+        let cosDLon = 1 - 2 * haverSinDLon
+        if (cosDLon <= 0) { return lat > 0 ? 90 : -90 }
+        return atan(tan(lat.degreesToRadians) / cosDLon).degreesToRadians
+    }
+}
---
+++ BirdBush.swift
@@ -0,0 +1,236 @@
+//
+//  BirdBush.swift
+//
+//  Created by Harry Netzer on 12/11/18.
+//
+//
+//  ISC License
+//
+//  Copyright (c) 2018, Vladimir Agafonkin
+//
+//  Permission to use, copy, modify, and/or distribute this software for any purpose
+//  with or without fee is hereby granted, provided that the above copyright notice
+//  and this permission notice appear in all copies.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+//  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+//  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+//  OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+//  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+//  THIS SOFTWARE.
+//
+
+import Foundation
+
+class BirdBush: Codable {
+    private(set) var ids = [Int]()
+    private(set) var coords = [Double]()
+    private(set) var nodeSize = 64
+    
+    init<T>(locations: [T], nodeSize: Int = 64, getID: (_ : T) -> Int, getX: (_ : T) -> Double, getY: (_ : T) -> Double) {
+        for location in locations {
+            ids.append(getID(location))
+            coords.append(getX(location))
+            coords.append(getY(location))
+        }
+        self.nodeSize = nodeSize
+        sortKD(left: 0, right: ids.count - 1, axis: 0)
+    }
+    
+    private func sortKD(left: Int, right: Int, axis: Int) {
+        if (right - left <= nodeSize) { return }
+    
+        let m = (left + right) >> 1 // middle index
+    
+        // sort ids and coords around the middle index so that the halves lie
+        // either left/right or top/bottom correspondingly (taking turns)
+        select(k: m, left: left, right: right, axis: axis)
+        
+        // recursively kd-sort first half and second half on the opposite axis
+        sortKD(left: left, right: m - 1, axis: 1 - axis)
+        sortKD(left: m + 1, right: right, axis: 1 - axis)
+    }
+    
+    // custom Floyd-Rivest selection algorithm: sort ids and coords so that
+    // [left..k-1] items are smaller than k-th item (on either x or y axis)
+    private func select(k: Int, left: Int, right: Int, axis: Int) {
+        var right = right
+        var left = left
+        
+        while (right > left) {
+            if (right - left > 600) {
+                let doublek = Double(k)
+                let n = Double(right - left + 1)
+                let m = Double(k - left + 1)
+                let z = log(Double(n))
+                let s = 0.5 * exp(2 * z / 3)
+                let sd = 0.5 * sqrt(z * s * (n - s) / n) * (m - n / 2 < 0 ? -1 : 1)
+                let newLeft = max(left, Int(floor(doublek - m * s / n + sd)))
+                let newRight = min(right, Int(floor(doublek + (n - m) * s / n + sd)))
+                select(k: k, left: newLeft, right: newRight, axis: axis)
+            }
+            
+            let t = coords[2 * k + axis]
+            var i = left
+            var j = right
+            
+            swapItem(left, k)
+            if (coords[2 * right + axis] > t) {
+                swapItem(left, right)
+            }
+            
+            while (i < j) {
+                swapItem(i, j);
+                i += 1
+                j -= 1
+                while (coords[2 * i + axis] < t) { i += 1 }
+                while (coords[2 * j + axis] > t) { j -= 1 }
+            }
+            
+            if (coords[2 * left + axis] == t) {
+                swapItem(left, j)
+            } else {
+                j += 1
+                swapItem(j, right)
+            }
+            
+            if (j <= k) { left = j + 1 }
+            if (k <= j) { right = j - 1 }
+        }
+    }
+    
+    func range(minX: Double, minY: Double, maxX: Double, maxY: Double) -> [Int] {
+        var stack = [0, ids.count - 1, 0]
+        var result = [Int]()
+    
+        // recursively search for items in range in the kd-sorted arrays
+        while (stack.count > 0) {
+            let axis = stack.popLast()!
+            let right = stack.popLast()!
+            let left = stack.popLast()!
+        
+            // if we reached a "tree node", search linearly
+            if (right - left <= nodeSize) {
+                for i in left...right {
+                    let x = coords[2 * i]
+                    let y = coords[2 * i + 1]
+                    if (x >= minX && x <= maxX && y >= minY && y <= maxY) {
+                        result.append(ids[i])
+                    }
+                }
+                continue
+            }
+        
+            // otherwise find the middle index
+            let m = (left + right) >> 1
+        
+            // include the middle item if it's in range
+            let x = coords[2 * m]
+            let y = coords[2 * m + 1]
+            if (x >= minX && x <= maxX && y >= minY && y <= maxY) {
+                result.append(ids[m])
+            }
+        
+            // queue search in halves that intersect the query
+            if (axis == 0 ? minX <= x : minY <= y) {
+                stack.append(left)
+                stack.append(m - 1)
+                stack.append(1 - axis)
+            }
+            if (axis == 0 ? maxX >= x : maxY >= y) {
+                stack.append(m + 1)
+                stack.append(right)
+                stack.append(1 - axis)
+            }
+        }
+        return result
+    }
+    
+    func within(qx: Double, qy: Double, r: Double) -> [Int] {
+        var stack = [0, ids.count - 1, 0]
+        var result = [Int]()
+        let r2 = r * r
+    
+        // recursively search for items within radius in the kd-sorted arrays
+        while (stack.count >= 3) {
+            let axis = stack.popLast()!
+            let right = stack.popLast()!
+            let left = stack.popLast()!
+        
+            // if we reached "tree node", search linearly
+            if (right - left <= nodeSize) {
+                for i in left...right {
+                    if (sqDist(coords[2 * i], coords[2 * i + 1], qx, qy) <= r2) {
+                        result.append(ids[i])
+                    }
+                }
+                continue
+            }
+        
+            // otherwise find the middle index
+            let m = (left + right) >> 1
+        
+            // include the middle item if it's in range
+            let x = coords[2 * m]
+            let y = coords[2 * m + 1]
+            if (sqDist(x, y, qx, qy) <= r2) {
+                result.append(ids[m])
+            }
+        
+            // queue search in halves that intersect the query
+            // this shit is so wrong
+            if (axis == 0 ? qx - r <= x : qy - r <= y) {
+                stack.append(left)
+                stack.append(m - 1)
+                stack.append(1 - axis)
+            }
+            if (axis == 0 ? qx + r >= x : qy + r >= y) {
+                stack.append(m + 1)
+                stack.append(right)
+                stack.append(1 - axis)
+            }
+        }
+    
+        return result
+    }
+    
+    private func sqDist(_ ax: Double, _ ay: Double, _ bx: Double, _ by: Double) -> Double {
+        let dx = (ax - bx)
+        let dy = (ay - by)
+        return dx * dx + dy * dy
+    }
+    
+    private func swapItem(_ i: Int, _ j: Int) {
+        ids.swapAt(i, j)
+        coords.swapAt(2*i, 2*j)
+        coords.swapAt(2*i+1, 2*j+1)
+    }
+    
+    /// how near could a point be to a given meridian or parallel?
+    /// Returns distance between a point and a line. But, the point is pushed further north
+    /// or south to make sure that the distance is minimal (points are closer to meridians at
+    /// the north pole).
+    private func orthoDist(qx: Double, qy: Double, line: Double, d: Int, r: Double) -> Double {
+        
+        let degreesLat = qy
+        
+        if d == 0 { // lon
+            let lat = degreesLat + r / 111694 // push lat up a lil bit
+            if lat >= 90 { return 0 }
+            
+            // Set up "Constants"
+            let p1 = 111412.84        // longitude calculation term 1
+            let p2 = -93.5            // longitude calculation term 2
+            
+            let latRad = degreesLat.degreesToRadians
+            
+            let lonlen = p1 * cos(latRad) + p2 * cos(3 * latRad)
+            return (line - qx) * lonlen
+        } else if d == 1 { // lat
+            return (line - degreesLat) * 110574
+        } else {
+            return 0
+        }
+    }
+}
